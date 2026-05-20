db775b5a86754b1a784693b89b7ed52a10bd4941
nanduzznandana154-collab <nanduzznandana154@gmail.com>
2026-05-20 12:45:17 +0530

    Initialize Lambda function with required imports

--- lambda2.py
+++ lambda2.py
@@ -2,24 +2,3 @@ import json
 import boto3
 import uuid
 
-s3 = boto3.client('s3')
-
-BUCKET_NAME = 'weather-00000001456'
-
-def lambda_handler(event, context):
-
-    for record in event['Records']:
-
-        data = record['dynamodb']['NewImage']
-
-        file_name = f"{uuid.uuid4()}.json"
-
-        s3.put_object(
-            Bucket=BUCKET_NAME,
-            Key=file_name,
-            Body=json.dumps(data)
-        )
-
-    return {
-        'statusCode': 200
-    }
