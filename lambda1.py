f4f9be1cf9c5b2831d22f6eb16d02a961bf4ad03
nanduzznandana154-collab <nanduzznandana154@gmail.com>
2026-05-20 12:40:36 +0530

    Add Lambda success response

--- lambda1.py
+++ lambda1.py
@@ -30,6 +30,11 @@ def lambda_handler(event, context):
     }
     table.put_item(Item=item)
 
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Weather data stored successfully')
+    }
+
 
 
 
