b434ffe28328c26cd6851bdbf0fc6f0468147828
nanduzznandana154-collab <nanduzznandana154@gmail.com>
2026-05-20 13:09:18 +0530

    Create Snowpipe for automatic ingestion from S3

--- snowflake.sql
+++ snowflake.sql
@@ -21,6 +21,17 @@ AWS_SECRET_KEY=os.getenv('AWS_SECRET_KEY')
 
 FILE_FORMAT = weather_json_format;
 
+create or replace pipe weather_pipe
+auto_ingest = TRUE
+as
+copy into weather_table
+from @weather_stage
+file_format=(
+type='json'
+)
+on_error=continue;
+
+
 
 
 
