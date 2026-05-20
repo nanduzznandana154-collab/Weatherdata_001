9ede471ee4ac5bce993e887e3f22ea66d3e8361f
nanduzznandana154-collab <nanduzznandana154@gmail.com>
2026-05-20 12:38:48 +0530

    Create weather record structure with timestamp

--- lambda1.py
+++ lambda1.py
@@ -22,6 +22,14 @@ def lambda_handler(event, context):
     temperature = str(data['list'][0]['main']['temp'])
     humidity = str(data['list'][0]['main']['humidity'])
 
+    item = {
+        'city': data['city']['name'],
+        'temperature': temperature,
+        'humidity': humidity,
+        'timestamp': datetime.now().isoformat()
+    }
+
+
 
 
 
