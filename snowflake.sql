ec1d3f10112df1bfd072f0fc3e4670f87a1007f6
nanduzznandana154-collab <nanduzznandana154@gmail.com>
2026-05-21 13:54:20 +0530

    Create Snowflake storage integration for S3 access

--- snowflake.sql
+++ snowflake.sql
@@ -8,35 +8,9 @@ CREATE OR REPLACE TABLE weather_table (
     data VARIANT
 );
 
-CREATE OR REPLACE FILE FORMAT weather_json_format
-TYPE = 'JSON';
-
-CREATE OR REPLACE STAGE weather_stage
-URL='s3://weather-00000001456/'
-
-CREDENTIALS=(
-AWS_KEY_ID=os.getenv('AWS_KEY_ID')
-AWS_SECRET_KEY=os.getenv('AWS_SECRET_KEY')
-)
-
-FILE_FORMAT = weather_json_format;
-
-create or replace pipe weather_pipe
-auto_ingest = TRUE
-as
-copy into weather_table
-from @weather_stage
-file_format=(
-type='json'
-)
-on_error=continue;
-
-show pipes;
-
-SELECT * FROM weather_table;
-
-
-
-
+CREATE STORAGE INTEGRATION s3_int
+TYPE = EXTERNAL_STAGE
+STORAGE_PROVIDER = S3
+ENABLED = TRUE
 
 
